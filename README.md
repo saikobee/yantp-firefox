cf49af1082eea6b2d570382b603273a2cbfde656
Brian Mock <brian@mockbrian.com>
2014-10-07 20:18:43 -0700

    Adds bottom bar to new tab page

--- README.md
+++ README.md
@@ -16,3 +16,12 @@ Install [`cfx`](https://developer.mozilla.org/en-US/Add-ons/SDK/Tutorials/Instal
 
 - `cfx xpi`
 - Drag XPI file onto Firefox
+
+## Thanks
+
+This is my first Firefox extension, and I couldn't have done it alone. Thanks
+to everyone who has worked on [the MDN add-on sdk documentation], and everyone
+who helped out in the Mozilla IRC `#jetpack` channel.
+
+Thanks especially to `zombie_` and `KWierso` for helping me figure out how to
+open the Firefox library organizer.
--- data/index.js
+++ data/index.js
@@ -10,6 +10,32 @@ const $root = {
         "toolbar",
         "unsorted"
     ],
+    otherLinks: [
+        {
+            url: "about:addons",
+            name: "Add-ons",
+        },
+        {
+            url: "about:preferences",
+            name: "Preferences",
+        },
+        {
+            url: "about:config",
+            name: "Config",
+        },
+        {
+            // action: "open-library",
+            url: "chrome://browser/content/places/places.xul",
+            name: "Library",
+        }
+    ],
+    handleLink: function(data) {
+        if ("url" in data) {
+            self.port.emit("set-url", { url: data.url });
+        } else if ("action" in data) {
+            self.port.emit(data.action);
+        }
+    },
     names: {
         menu: "Menu",
         toolbar: "Toolbar",
