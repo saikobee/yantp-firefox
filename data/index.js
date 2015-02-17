8740457041e1975678e6dc83b8b60ec8ca98b108
Brian Mock <bmock@janrain.com>
2015-02-16 20:21:23 -0800

    WIP speeding things up

--- data/index.js
+++ data/index.js
@@ -1,52 +1,177 @@
-const $root = {
-    bookmarks: {
-        menu: ko.observableArray(),
-        toolbar: ko.observableArray(),
-        unsorted: ko.observableArray()
-    },
-    current: ko.observable("menu"),
-    tabs: [
-        "menu",
-        "toolbar",
-        "unsorted"
-    ],
-    otherLinks: [
-        {
-            url: "about:addons",
-            name: "Add-ons",
-        },
-        {
-            url: "about:preferences",
-            name: "Preferences",
-        },
-        {
-            url: "about:config",
-            name: "Config",
-        },
-        {
-            // action: "open-library",
-            url: "chrome://browser/content/places/places.xul",
-            name: "Library",
-        }
-    ],
-    handleLink: function(data) {
-        if ("url" in data) {
-            self.port.emit("set-url", { url: data.url });
-        } else if ("action" in data) {
-            self.port.emit(data.action);
-        }
-    },
-    names: {
-        menu: "Bookmarks Menu",
-        toolbar: "Bookmarks Toolbar",
-        unsorted: "Unsorted Bookmarks"
-    }
+const port = self.port;
+
+const bookmarks = {
+    menu: [],
+    toolbar: [],
+    unsorted: []
 };
 
-$root.tabs.forEach(function(name) {
-    self.port.on("set-bookmarks-" + name, function(marks) {
-        $root.bookmarks[name](marks);
-    });
-});
+const bookmarkTypes = Object.keys(bookmarks);
 
-ko.applyBindings($root);
+function setChildren(elem, children) {
+    elem.innerHTML = "";
+    children.forEach(function(kid) {
+        elem.appendChild(coerceToNode(kid));
+    });
+}
+
+function fillContainer(name) {
+    const frag = document.createDocumentFragment();
+    const elems = bookmarks[name].map(bookmarkElement);
+    setChildren(frag, elems);
+    setChildren(getContainer(name), [frag]);
+}
+
+function getContainer(name) {
+    return document.getElementById("container-" + name);
+}
+
+function getTab(name) {
+    return document.getElementById("button-" + name);
+}
+
+function showContainer(name) {
+    getContainer(name).style.display = "";
+    getTab(name).classList.add("active");
+}
+
+function hideContainer(name) {
+    getContainer(name).style.display = "none";
+    getTab(name).classList.remove("active");
+}
+
+function hideAllContainers() {
+    bookmarkTypes.forEach(hideContainer);
+}
+
+function concat(a, b) {
+    return a.concat(b);
+}
+
+function flatten(ary) {
+    return ary.reduce(concat, []);
+}
+
+function setUrl(url) {
+    port.emit("set-url", { url: url });
+}
+
+function coerceToNode(textOrNode) {
+    return typeof textOrNode === "string"
+        ? document.createTextNode(textOrNode)
+        : textOrNode;
+}
+
+function assignIfHas(dest, src) {
+    return function(key) {
+        if (key in src) {
+            dest[key] = src[key];
+        }
+    };
+}
+
+function E(opts) {
+    const type = opts.type || "span";
+    const name = opts.className || "";
+    const href = opts.href || "";
+    const src = opts.src || "";
+    const kids = opts.children || [];
+    const elem = document.createElement(type);
+    const props = ["className", "href", "src"];
+    props.forEach(assignIfHas(elem, opts));
+    setChildren(elem, kids);
+    return elem;
+}
+
+function T(name, text) {
+    return E({
+        type: "span",
+        className: name,
+        children: [text]
+    });
+}
+
+function bookmarkElement(data) {
+    return E({
+        type: "a",
+        className: "bookmark",
+        href: data.url,
+        children: flatten([
+            bookmarkFaviconElement(data),
+            bookmarkPrefixElements(data),
+            bookmarkTitleElement(data)
+        ])
+    });
+}
+
+function bookmarkFaviconElement(data) {
+    return E({
+        type: "img",
+        className: "favicon",
+        src: data.favicon
+    })
+}
+
+function bookmarkPrefixElements(data) {
+    return flatten(data.prefix.map(function(prefix) {
+        return [
+            T("folder", prefix),
+            T("separator", "/")
+        ];
+    }));
+}
+
+function bookmarkTitleElement(data) {
+    return T("title", data.title);
+}
+
+function whenClicking(id, handler) {
+    document
+        .getElementById(id)
+        .addEventListener("click", handler, false);
+}
+
+function listenForTab(type) {
+    whenClicking("button-" + type, function(event) {
+        hideAllContainers();
+        showContainer(type);
+    });
+}
+
+function installPrivilegedLink(id, url) {
+    whenClicking(id, function() {
+        port.emit("set-url", { url: url });
+    });
+}
+
+function installListeners() {
+    port.on("set-bookmarks", function(marks) {
+        bookmarkTypes.forEach(function(name) {
+            bookmarks[name] = marks[name];
+        });
+        render();
+    });
+    bookmarkTypes.forEach(listenForTab);
+    installPrivilegedLink("other-addons", "about:addons");
+    installPrivilegedLink("other-preferences", "about:preferences");
+    installPrivilegedLink("other-config", "about:config");
+}
+
+function getBookmarks() {
+    port.emit("get-bookmarks", {});
+}
+
+function render() {
+    hideAllContainers();
+    fillContainer("menu");
+    fillContainer("toolbar");
+    fillContainer("unsorted");
+    showContainer("menu");
+}
+
+function main() {
+    installListeners();
+    getBookmarks();
+}
+
+main();
